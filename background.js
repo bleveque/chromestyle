7225f884b0813c8e85441a06e9690da758959090
Ben LeVeque <ben.leveque@gmail.com>
2014-01-29 01:43:23 -0500

    added some files

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.browserAction.onClicked.addListener(function() {
+	chrome.tabs.executeScript(null, {
+		file: 'content_script.js'
+	});
+});
---
+++ content_script.js
@@ -0,0 +1,2 @@
+document.body.style.fontFamily = 'sans-serif';
+document.body.style.fontSize = '17px';
